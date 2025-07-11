bc30bb542412a57c97ebf3467687df749281d6e0
ShuaiBai <374381746@qq.com>
2025-07-11 15:45:47 +0800

    init commit

---
+++ 84.柱状图中最大的矩形.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=cpp
+ *
+ * [84] 柱状图中最大的矩形
+ *
+ * https://leetcode.cn/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (47.50%)
+ * Likes:    2950
+ * Dislikes: 0
+ * Total Accepted:    542.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * 
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 
+ * 
+ * 输入：heights = [2,1,5,6,2,3]
+ * 输出：10
+ * 解释：最大的矩形为图中红色区域，面积为 10
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入： heights = [2,4]
+ * 输出： 4
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+    }
+};
+// @lc code=end
+
